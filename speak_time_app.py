ef684c886f93f7b5bee75c36e63890854a1127ad
meenu517 <43227753+meenu517@users.noreply.github.com>
2024-12-01 06:50:41 -0600

    Update speak_time_app.py

--- speak_time_app.py
+++ speak_time_app.py
@@ -32,6 +32,7 @@ if st.button("Start Time Announcements"):
     # start_time_announcement()
     current_time = time.strftime("%I:%M %p")  # Format as "03:15 PM"
     text = f"The current time is {current_time}"
+    st.write(f"text is {text}")
     tts = gTTS(text=text, lang='en')
     tts.save("time.mp3")
     playsound("time.mp3")
