a3a81781a68d61089802a365e35a78c12fae2a0f
meenu517 <43227753+meenu517@users.noreply.github.com>
2024-12-01 06:09:31 -0600

    Update speak_time_app.py

--- speak_time_app.py
+++ speak_time_app.py
@@ -3,26 +3,26 @@ import streamlit as st
 import pyttsx3
 import time
 import threading
-from playsound import playsound
+# from playsound import playsound
 
 
 # Initialize text-to-speech engine
 # engine = pyttsx3.init()
 
 # Function to speak the time
-def speak_time_gtts():
-    while True:
-        current_time = time.strftime("%I:%M %p")  # Format as "03:15 PM"
-        text = f"The current time is {current_time}"
-        tts = gTTS(text=text, lang='en')
-        tts.save("time.mp3")
-        playsound("time.mp3")
-        time.sleep(1 * 60)  # Wait for 5 minutes
+# def speak_time_gtts():
+#     while True:
+#         current_time = time.strftime("%I:%M %p")  # Format as "03:15 PM"
+#         text = f"The current time is {current_time}"
+#         tts = gTTS(text=text, lang='en')
+#         tts.save("time.mp3")
+#         playsound("time.mp3")
+#         time.sleep(1 * 60)  # Wait for 5 minutes
 
-# Background thread to run the speak_time function
-def start_time_announcement():
-    thread = threading.Thread(target=speak_time_gtts, daemon=True)
-    thread.start()
+# # Background thread to run the speak_time function
+# def start_time_announcement():
+#     thread = threading.Thread(target=speak_time_gtts, daemon=True)
+#     thread.start()
 
 # Streamlit UI
 st.title("Time Announcer")
